32f592874fbab3cc9d2e9abab1a237402c26cd7e
Sittrinon <60164369+Sittrinon@users.noreply.github.com>
2021-12-16 02:53:52 +0700

    Update code_eda.R

--- code_eda.R
+++ code_eda.R
@@ -9,30 +9,50 @@ StudentsPerformance<-read_csv("https://raw.githubusercontent.com/sit-2021-int214
 View(StudentsPerformance)
 
 
+#5.raceใดที่มีคะแนนเฉลี่ยในmath score
+StudentsPerformance%>%group_by(`race/ethnicity`)%>%
+summarise(avg=mean(`math score`))%>%
+arrange(desc(`race/ethnicity`))%>%head(1)
+
+#6.คนที่เข้าคอร์สเตรียมสอบมีคะแนนวิชาreadingเฉลี่ยเท่าไหร่
+
+StudentsPerformance%>%group_by(`test preparation course`)%>%
+summarise(avg=mean(`reading score`))%>%  
+filter(`test preparation course` == 'completed')  
+
+
+#7.ระหว่างคนที่เข้าคอร์สเตรียมสอบและไม่ได้เตรียมสอบมีคะแนนเฉลี่ยวิชาwritingเท่าไหร่
+StudentsPerformance%>%group_by(`test preparation course`)%>%
+  summarise(avg=mean(`writing score`))%>%  
+  filter(`test preparation course` == 'completed')  
+
+
+
+StudentsPerformance%>%group_by(`test preparation course`)%>%
+  summarise(avg=mean(`writing score`))%>%  
+  filter(`test preparation course` == 'none')
+  
+  
+
+#8.แต่ละ parental level of education มี lunch แบบละเท่าใด
+
+
+StudentsPerformance%>%group_by(`parental level of education`)%>%
+count(lunch)  
 
 
 
 
+##graph1
+StudentsPerformance %>% ggplot(aes(x=`gender`))+
+  geom_bar(aes(fill= `lunch`))
 
+##graph2
+g <- ggplot(StudentsPerformance,aes(x="",y=`gender`,fill= `parental level of education`))+
+  geom_bar(width = 1, stat = "identity")
+g
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pie<-g + coord_polar("y", start=1)
+pie + theme(axis.text.x=element_blank())+
+scale_fill_brewer(palette="Set2")
 
