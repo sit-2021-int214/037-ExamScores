41b3c5cf6e0f7d0875eba153a23c7fe3979f695d
Sittrinon <60164369+Sittrinon@users.noreply.github.com>
2021-12-17 15:07:19 +0700

    Update code_eda.R

--- code_eda.R
+++ code_eda.R
@@ -3,7 +3,7 @@
 library(dplyr)
 library(readr)
 library(ggplot2)
-
+library(tidyverse)
 StudentsPerformance<-read_csv("https://raw.githubusercontent.com/sit-2021-int214/037-StudentsPerformance/main/StudentsPerformance_original.csv")
 View(StudentsPerformance)
 
